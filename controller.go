996820cc9cd5c0bd6e7c346575ca695b9852e99d
Heiko Scherrer <scherrer@openwms.org>
2018-07-01 11:42:49 +0200

    pin config

--- controller.go
+++ controller.go
@@ -60,8 +60,8 @@ var (
 
 func init() {
 	fmt.Printf("Initializing...\n")
-	initLoggers(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
-	//initLoggers(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
+	//initLoggers(os.Stdout, os.Stdout, os.Stdout, os.Stderr)
+	initLoggers(ioutil.Discard, os.Stdout, os.Stdout, os.Stderr)
 	//initGpio()
 }
 
